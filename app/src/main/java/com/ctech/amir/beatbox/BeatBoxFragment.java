50af8455a54adca9638948f3ebc787e59b625b16
amiromrani <46926811+amiromrani@users.noreply.github.com>
2019-04-30 08:58:54 -0500

    first unit beatbox

--- app/src/main/java/com/ctech/amir/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/ctech/amir/beatbox/BeatBoxFragment.java
@@ -1,11 +1,31 @@
 package com.ctech.amir.beatbox;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ctech.amir.beatbox.databinding.FragmentBeatBoxBinding;
 
 
 public class BeatBoxFragment extends Fragment {
 
     public static BeatBoxFragment newInstance() {
         return new BeatBoxFragment();
+    }
+
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                       Bundle savedInstanceState) {
+        // use the DataBindingUtil to inflate our layout
+        FragmentBeatBoxBinding binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_beat_box, container, false);
+
+        // configure the recycler view to use a gridlayoutManager with 3 colums
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        return binding.getRoot();
     }
 }
