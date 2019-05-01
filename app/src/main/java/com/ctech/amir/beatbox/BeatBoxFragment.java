cf98c0658f84d44eba737dd49dd9dfd06bb7fb06
amiromrani <46926811+amiromrani@users.noreply.github.com>
2019-05-01 08:35:33 -0500

    added sounds

--- app/src/main/java/com/ctech/amir/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/ctech/amir/beatbox/BeatBoxFragment.java
@@ -2,13 +2,16 @@ package com.ctech.amir.beatbox;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.ctech.amir.beatbox.databinding.FragmentBeatBoxBinding;
+import com.ctech.amir.beatbox.databinding.ListItemSoundBinding;
 
 
 public class BeatBoxFragment extends Fragment {
@@ -25,7 +28,47 @@ public class BeatBoxFragment extends Fragment {
 
         // configure the recycler view to use a gridlayoutManager with 3 colums
         binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        binding.recyclerView.setAdapter(new SoundAdapter());
 
         return binding.getRoot();
     }
+
+    // Create a view holder for individual sound buttons
+    private class SoundHolder extends RecyclerView.ViewHolder {
+        private ListItemSoundBinding mBinding;
+
+        private SoundHolder(ListItemSoundBinding binding) {
+            super(binding.getRoot());
+            mBinding = binding;
+        }
+    }
+
+    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder>{
+        @NonNull
+        @Override
+        public SoundHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            ListItemSoundBinding binding = DataBindingUtil.inflate(
+                    inflater, R.layout.list_item_sound, viewGroup, false);
+
+            return new SoundHolder(binding);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull SoundHolder soundHolder, int i) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
+
+
+
+
+
+
 }
